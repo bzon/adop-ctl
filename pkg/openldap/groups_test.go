37a24169228a7cf7d1d7fcb7751147adfb90b932
Miguel Tanada <miguel.g.tanada@accenture.com>
2018-04-18 13:21:34 +0800

    Initial OpenLdap(group sync only) Development

--- pkg/openldap/groups_test.go
+++ pkg/openldap/groups_test.go
@@ -1,9 +1,65 @@
 package openldap
 
 import (
+	"fmt"
+	"os"
 	"testing"
+
+	"github.com/bzon/adop-ctl/pkg/gitlab"
 )
 
+var openldap = &Client{
+	Host:   "localhost",
+	Scheme: "tcp",
+	Port:   389,
+}
+
+var GitlabAPI = gitlab.API{
+	HostURL: "http://localhost:10080/api/v4",
+	Token:   os.Getenv("GITLAB_PRIVATE_TOKEN"),
+}
+
+func TestGetGroup(t *testing.T) {
+
+	// get nx-admin and administrators group
+	group, err := openldap.GetGroup("dc=ldap,dc=adop,dc=com", "administrators", "nx-admin")
+	if err != nil {
+		fmt.Printf("error: %v\n", err)
+	}
+
+	// print results
+	for i := range group {
+		for j := 0; j < len(group[i].uniqueMembers); j++ {
+			fmt.Printf("Group: %s, Member: %s\n", group[i].cn, group[i].uniqueMembers[j])
+		}
+	}
+
+}
+func TestGetGroupList(t *testing.T) {
+
+	// test get group list
+	groups, err := openldap.GetGroupList("dc=ldap,dc=adop,dc=com")
+	if err != nil {
+		fmt.Printf("error: %v\n", err)
+	}
+
+	// print results
+	for j := 0; j < len(groups); j++ {
+		fmt.Println("Groups:" + groups[j])
+	}
+
+}
 func TestSyncGroup(t *testing.T) {
 
+	// get group list
+	groupList, err := openldap.GetGroupList("dc=ldap,dc=adop,dc=com")
+	if err != nil {
+		fmt.Printf("error: %v\n", err)
+	}
+
+	// get groups
+	groups, err := openldap.GetGroup("dc=ldap,dc=adop,dc=com", groupList...)
+
+	// sync groups
+	openldap.SyncGitlabGroup(groups, GitlabAPI)
 }
